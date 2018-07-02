5105d7d8f416b2b9febe4e1267d08cd296456eb6
VirtualRcoder <shubhamsh2820@gmail.com>
2018-07-03 01:07:44 +0530

    Creating pr

--- Feedback/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.conf.urls import url
-
-urlpatterns=[
-	url(r'^Login',views.login),
-	url(r'^Register',views.register),
-]
--- Feedback/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def login(request):
-    return render(request,'Feedback/Login.html')
-
-def register(request):
-    return render(request,'Feedback/Register.html')
-
