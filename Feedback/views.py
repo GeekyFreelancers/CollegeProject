7be8aee97061751ee1b94f5545f5580b1ce635e2
gutsytechster <prashantsharma161198@gmail.com>
2018-08-20 21:29:40 +0530

    feat: Add template to show the feedback form
    This adds the initial template for feedback form.
    It uses the form defined in forms.py through views.py.
    The template is dynamic and show form for specific section defined in url.
    It shows the teacher and their respective subjects as per given section.

--- Feedback/views.py
+++ Feedback/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.forms import formset_factory
+from .forms import FeedbackForm
+from .models import Section, Subject, Teacher, TeacherSubjectMapping
 
 # Create your views here.
+def Feedback_form_Page(request, class_name):
+	section = get_object_or_404(Section, section_name=class_name)
+	teacher_with_subject = section.subjects.all()
+	feedback_formset = formset_factory(FeedbackForm, extra=teacher_with_subject.count())
+	Formset = feedback_formset()
+	Iterators = zip(teacher_with_subject, Formset)
+	return render(request, 'Feedback/feedback.html', {'Iterators': Iterators})
