9644712b7d24c4d80336d1b9df7c561284e330cc
inquiridortechie <narya436.na@gmail.com>
2018-08-26 01:41:17 +0530

    refactor: Update models.py

--- Feedback/models.py
+++ Feedback/models.py
@@ -30,14 +30,26 @@ class Section(models.Model):
 	section_name = models.CharField(max_length=3)
 	year = models.IntegerField(validators=[
 		MaxValueValidator(4),
-		MinValueValidator(1)
-		])
+		MinValueValidator(1) ])
 	branch = models.CharField(max_length=20)
 	shift = models.IntegerField(validators=[
 		MaxValueValidator(2),
-		MinValueValidator(1)
-		])
+		MinValueValidator(1) ])
 	subjects = models.ManyToManyField(TeacherSubjectMapping)
 
 	def __str__(self):
 		return self.section_name
+
+
+class Feedback(models.Model):
+	teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+	section = models.ForeignKey(Section, on_delete=models.CASCADE, null=True)
+	Punctuality = models.IntegerField(blank=False)
+	Subject_knowledge = models.IntegerField(blank=False)
+	Behaviour = models.IntegerField(blank=False)
+	Method_of_teaching = models.IntegerField(blank=False)
+	Audibility = models.IntegerField(blank=False)
+	Syllabus_coverage = models.IntegerField(blank=False)
+
+	def __str__(self):
+		return f"{str(self.teacher)}-{str(self.section)}"
