57a33692b7cdbee3f25f9f81ad3c944743797f8e
gutsytechster <prashantsharma161198@gmail.com>
2018-09-14 12:20:55 +0530

    feat(views.py): Save feedback to the database
    Updated views.py saves the filled feedback to the database.
    This also adds the file utils.py which provides functionalities
    to the views.

--- Feedback/views.py
+++ Feedback/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.forms import formset_factory
-from .forms import FeedbackForm
-from .models import Section, Subject, Teacher, TeacherSubjectMapping
 
-# Create your views here.
+from .forms import FeedbackForm
+from .models import Section
+from .utils import form_is_incomplete, update_or_create_feedback_instance
+
+
 def Feedback_form_Page(request, class_name):
-	section = get_object_or_404(Section, section_name=class_name)
-	teacher_with_subject = section.subjects.all()
-	feedback_formset = formset_factory(FeedbackForm, extra=teacher_with_subject.count())
-	Formset = feedback_formset()
-	Iterators = zip(teacher_with_subject, Formset)
-	return render(request, 'Feedback/feedback.html', {'Iterators': Iterators})
+    section = get_object_or_404(Section, section_name=class_name)
+    teacher_with_subject = section.subjects.all()
+    # It creates the no. of forms equals to the no. of teachers
+    feedback_formset = formset_factory(
+        FeedbackForm, extra=teacher_with_subject.count())
+    error = False
+    if request.method == 'POST':
+        Formset = feedback_formset(request.POST)
+        if Formset.is_valid():
+            # It contains the `list of forms` in form of `list of dictionaries`
+            actual_data = Formset.cleaned_data
+            if form_is_incomplete(actual_data):
+                Iterators = zip(teacher_with_subject, Formset)
+                return render(
+                    request, 'Feedback/feedback.html',
+                    {'Iterators': Iterators, 'formset': Formset, 'error': True}
+                )
+            update_or_create_feedback_instance(actual_data,
+                                               teacher_with_subject, section)
+            return HttpResponseRedirect('/')
+        else:
+            error = True
+    else:
+        Formset = feedback_formset()
+    Iterators = zip(teacher_with_subject, Formset)
+    return render(
+        request, 'Feedback/feedback.html',
+        {'Iterators': Iterators, 'formset': Formset, 'error': error})
