c4b81eed16301b7e7954dd71aef4826269a3ad94
SHUBHAM SHARMA <shubhamsh2820@gmail.com>
2018-10-07 21:36:03 +0530

    feat: Show the total feedback of a teacher with given teacher name and department name (#28)
    It allows to see the total feedback of every available teacher by providing teacher's name
    and his/her department's name.
    It also includes respective html and css files.

--- Feedback/urls.py
+++ Feedback/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('feedback/<str:class_name>/', views.Feedback_form_Page),
+    path('', views.Feedback_submit, name='submit'),
+    path('t', views.teacher_check, name='teacher_check'),
 ]
